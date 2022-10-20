dd619e4a1d4a0756582b547520c9ad7169499044
shimoda <kuri65536@hotmail.com>
2022-10-21 07:35:40 +0900

    convert `gtk_application_run` and `g_object_unref`.

--- simple.nim
+++ simple.nim
@@ -26,6 +26,12 @@ type
 
 proc gtk_application_new(class_string: cstring, flags: GApplicationFlags
                          ): GtkApplicationPtr {.importc: "gtk_application_new".}
+
+proc g_object_unref(app: GtkApplicationPtr): void {.importc.}
+
+proc g_application_run(app: GtkApplicationPtr,
+                       argc: int, argv: openarray[cstring]): int {.importc.}
+
 #[
 
 static void
@@ -42,16 +48,12 @@ activate (GtkApplication* app,
 
 
 proc main(argc: int, argv: openarray[cstring]): int =
-  #[
-  int status;
-  ]#
   var app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE)
   #[
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
   ]#
-  let status = 0
+  let status = g_application_run(app, argc, argv)
+  g_object_unref (app);
   return status;
 
 
